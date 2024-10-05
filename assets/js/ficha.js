cc5f1477a712effe76bf3fe96ed2c82b0ac24313
leletgameplays <leticiabarbosa9678@gmail.com>
2024-10-04 22:27:22 -0300

    AEEEEE

--- assets/js/ficha.js
+++ assets/js/ficha.js
@@ -1,40 +1,24 @@
-export function rolarDados() {
+let atributos = ['forca', 'agilidade', 'inteligencia', 'carisma', 'constituicao'];
+let indiceAtual = 0;
+
+function rolarDados() {
     let valor = Math.floor(Math.random() * 20) + 1;
-    console.log(valor);
-    return valor;
+
+    const resultados = [-5, -5, -3, -3, -2, -2, -1, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 5, 5];
+    resultado = resultados[valor - 1];
+
+    return resultado;
 }
 
-/* if (valor >= 1 && valor <= 3) {
-        resultado = -5;
-    } else if (valor >= 2 && valor <= 5) {
-        resultado = -3;
-    } else if (valor >= 4 && valor <= 7) {
-        resultado = -2;
-    } else if (valor >= 6 && valor <= 9) {
-        resultado = -1;
-    } else if (valor >= 8 && valor <= 11) {
-        resultado = 0;
-    } else if (valor >= 10 && valor <= 13) {
-        resultado = 1;
-    } else if (valor >= 12 && valor <= 15) {
-        resultado = 2;
-    } else if (valor >= 14 && valor <= 17) {
-        resultado = 3;
-    } else if (valor >= 16 && valor <= 19) {
-        resultado = 4;
-    } else if (valor >= 18 && valor <= 21) {
-        resultado = 5;
+function rolarAtributo() {
+    if (indiceAtual < atributos.length) {
+        let pericia = atributos[indiceAtual];
+        let valor = rolarDados();
+        document.getElementById(pericia).value = valor;
+        // pericia se torna o indice de algum atributo
+        /*Ex: o indice é 1, agilidade, então no HTML onde está o id agilidade, vai se tornar o valor que o dado tirou*/
+        indiceAtual++;
+    } else {
+        alert("Todos os atributos já foram preenchidos!");
     }
-
-    console.log(Valor gerado: ${valor}, Resultado: ${resultado});
-    return resultado; resuma o codigo*/
-
-/*// Array de resultados baseado no valor gerado (1 a 20)
-    const resultados = [-5, -5, -3, -3, -2, -2, -1, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 5, 5];
-
-    // O valor gerado é usado como índice para o array de resultados
-    resultado = resultados[valor - 1]; // valor - 1 porque os índices do array começam em 0
-
-    console.log(`Valor gerado: ${valor}, Resultado: ${resultado}`);
-    return resultado;
-}*/
+}
---
+++ assets/js/img/mesoregiaoimg.js
@@ -0,0 +1,6 @@
+const imgmeso = {
+    centralpotiguar: "https://live.staticflickr.com/8014/7490170364_8d88b85271_b.jpg",
+    oestepotiguar: "https://blog.tuaterra.com.br/wp-content/uploads/2019/03/mossoro.jpg",
+    agrestepotiguar: "https://novo.grandeponto.com.br/storage/backup/noticias/202108121200590DHtNL.jpg",
+    lestepotiguar: "https://blogdeviagens.com.br/wp-content/uploads/2022/05/forte-1-984x600.png"
+}
